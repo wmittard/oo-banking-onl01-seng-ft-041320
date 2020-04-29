d14471bd866c8a02159306ea191fc8aa9ae0a42a
William Mittard <billymittard@ymail.com>
2020-04-29 17:26:06 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
+attr_accessor :balance, :status
+attr_reader :name
 
-end
+ def initialize(name)
+   @name = name  
+   @balance = 1000
+   @status = "open"
+ end 
+ 
+ def deposit(amount)
+   @balance += amount
+ end
+
+ def display_balance
+   "Your balance is $#{@balance}."
+ end 
+ 
+ def valid?
+   self.balance > 0 && self.status == "open" ? true : false 
+ end 
+
+ def close_account
+   @status = "closed"
+ end
+ 
+ end 
+
